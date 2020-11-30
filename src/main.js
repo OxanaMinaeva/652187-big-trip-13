4758e42e0668e6b18a7355989d3ae3178b8a3868
Oxana Minaeva <oxdikova@gmail.com>
2020-11-30 23:57:15 +0300

    Реализация 5. Революция или эволюция? (часть 1)

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
 import dayjs from "dayjs";
-import {createTripInfoTemplate} from "./view/trip-info.js";
-import {createTripCostTemplate} from "./view/trip-cost.js";
-import {createSiteMenuTemplate} from "./view/site-menu.js";
-import {createFilterTemplate} from "./view/filter.js";
-import {createTripSortTemplate} from "./view/trip-sort.js";
-import {createEditPointTemplate} from "./view/edit-point.js";
-import {createNewPointTemplate} from "./view/new-point.js";
-import {createTripEventTemplate} from "./view/trip-event.js";
-import {createFullOfferTemplate} from "./view/full-offer.js";
 import {generateEvent} from "./mock/event.js";
 
+import {render, RenderPosition} from "./utils.js";
+import SiteMenuView from "./view/site-menu.js";
+import FilterView from "./view/filter.js";
+import EditPointView from "./view/edit-point.js";
+import FullOfferView from "./view/full-offer.js";
+import TripCostView from "./view/trip-cost.js";
+import TripEventView from "./view/trip-event.js";
+import TripInfoView from "./view/trip-info.js";
+import TripSortView from "./view/trip-sort.js";
+
+
 const TASK_COUNT = 20;
-const FIRST_TASK = 1;
 const events = new Array(TASK_COUNT).fill().map(generateEvent);
 const destinations = [];
 const dates = [];
 let cost = 0;
 const sortEvents = events.sort((a, b) => dayjs(a.date) > dayjs(b.date) ? 1 : -1);
 
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const siteMainElement = document.querySelector(`.trip-main`);
 const siteHeaderElement = siteMainElement.querySelector(`.trip-controls`);
@@ -30,41 +27,54 @@ const siteBodyElement = document.querySelector(`.page-body__page-main`);
 const siteEventElement = siteBodyElement.querySelector(`.trip-events`);
 
 
-render(siteEventElement, createTripSortTemplate(), `afterbegin`);
-render(siteEventElement, createEditPointTemplate(sortEvents[0]), `beforeend`);
+render(siteEventElement, new TripSortView().getElement(), RenderPosition.AFTERBEGIN);
 
-const siteEditElement = siteEventElement.querySelector(`.event--edit`);
-render(siteEditElement, createFullOfferTemplate(sortEvents[0]), `beforeend`);
-render(siteEventElement, createNewPointTemplate(sortEvents[1]), `beforeend`);
+// Рендер точки маршрута и редактирования
+const renderEvent = (event) => {
+  const tripEventComponent = new TripEventView(event);
+  const tripEventEditComponent = new EditPointView(event);
+  const tripEventOfferComponent = new FullOfferView(event);
 
+  render(siteEventElement, tripEventComponent.getElement(), RenderPosition.BEFOREEND);
 
-for (let i = FIRST_TASK; i < TASK_COUNT; i++) {
-  render(siteEventElement, createTripEventTemplate(sortEvents[i]), `beforeend`);
-  cost += Number(sortEvents[i].price);
-  destinations.push(sortEvents[i].destination);
-  dates.push(sortEvents[i].date);
+  const replaceEventToEdit = () => {
+    siteEventElement.replaceChild(tripEventEditComponent.getElement(), tripEventComponent.getElement());
+    render(tripEventEditComponent.getElement().lastElementChild.lastElementChild, tripEventOfferComponent.getElement(), RenderPosition.BEFOREEND);
+  };
+
+  const replaceEditToEvent = () => {
+    siteEventElement.replaceChild(tripEventComponent.getElement(), tripEventEditComponent.getElement());
+  };
+
+  tripEventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    replaceEventToEdit();
+  });
+
+  tripEventEditComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    replaceEditToEvent();
+  });
+
+  tripEventEditComponent.getElement().querySelector(`form`).addEventListener(`submit`, function (evt) {
+    evt.preventDefault();
+    replaceEditToEvent();
+  });
+};
+
+const tripMainInfo = (event) => {
+  cost += Number(event.price);
+  destinations.push(event.destination);
+  dates.push(event.date);
+};
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  renderEvent(sortEvents[i]);
+  tripMainInfo(sortEvents[i]);
 }
 
-render(siteMainElement, createTripCostTemplate(cost), `afterbegin`);
-render(siteMainElement, createTripInfoTemplate(destinations, dates), `afterbegin`);
-render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
-render(siteHeaderElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, new TripCostView(cost).getElement(), RenderPosition.AFTERBEGIN);
+render(siteMainElement, new TripInfoView(destinations, dates).getElement(), RenderPosition.AFTERBEGIN);
 
-
-const rollupButton = siteEditElement.querySelector(`.event__rollup-btn`);
-const resetButton = siteEditElement.querySelector(`.event__reset-btn`);
-
-rollupButton.addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  siteEditElement.remove();
-});
-
-resetButton.addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  siteEditElement.remove();
-});
-
-siteEditElement.addEventListener(`submit`, function (evt) {
-  evt.preventDefault();
-  siteEditElement.remove();
-});
+render(siteHeaderElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, new FilterView().getElement(), RenderPosition.BEFOREEND);
--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,18 +1,36 @@
-export const createTripInfoTemplate = (destinations, dates) => {
-  let info;
-  let startDate;
-  let endDate;
+import {createElement} from "../utils.js";
+import dayjs from "dayjs";
 
-  if (destinations.length > 3) {
-    info = destinations[0] + ` - ` + destinations.pop();
-    startDate = dates[0];
-    endDate = dates.pop();
-  }
+const createTripInfoTemplate = (destinations, dates) => {
   return `<div class="trip-main">
             <section class="trip-main__trip-info  trip-info">
               <div class="trip-info__main">
-                <h1 class="trip-info__title">${info}</h1>
+                <h1 class="trip-info__title">${destinations.length > 3 ? destinations[0] + ` -...- ` + destinations.pop() : destinations.join(` - `)}</h1>
 
-                <p class="trip-info__dates">${startDate.format(`MMM DD`)}&nbsp;&mdash;&nbsp;${endDate.format(`MMM DD`)}</p>
+                <p class="trip-info__dates">${dayjs(dates[0]).format(`MMM DD`)}&nbsp;&mdash;&nbsp;${dayjs(dates.pop()).format(`MMM DD`)}</p>
               </div>`;
 };
+
+export default class TripInfo {
+  constructor(destinations, dates) {
+    this._destinations = destinations;
+    this._dates = dates;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripInfoTemplate(this._destinations, this._dates);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
